bb0ceccddbf89ea3f82dd94ac27d1ebb65af778d
Daniel Roe <daniel@roe.dev>
2022-10-09 21:59:02 +0100

    chore: try explicit config

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,5 +2,11 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/website',
   build: {
     transpile: [/content-edge/]
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/']
+    }
   }
 })
